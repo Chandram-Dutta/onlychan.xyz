41008d9901322d17182103dcff74a8ea6f6d3005
Chandram-Dutta <chandramdutta2004@gmail.com>
2025-03-14 15:35:15 +0530

    added ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import node from '@astrojs/node';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
+  server: {
+    host: '0.0.0.0'
+  },
+  integrations: [mdx(), sitemap()]
 });
