6265500cf83c61c906883a8f3281f29fbd980b7f
Chandram-Dutta <chandramdutta2004@gmail.com>
2025-03-14 16:12:25 +0530

    feat: added endpoints

---
+++ src/pages/api/work.ts
@@ -0,0 +1,9 @@
+import type { APIRoute } from 'astro';
+import fs from 'fs';
+import path from 'path';
+
+export const GET: APIRoute = ({ params, request }) => {
+    const filePath = path.resolve('src/assets/work.json');
+    const jsonData = fs.readFileSync(filePath, 'utf-8');
+    return new Response(jsonData);
+}
